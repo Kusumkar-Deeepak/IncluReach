8d9b37c59433c0bc73c0993d3abe38de45a4920a
Kusumkar-Deeepak <deeepak.kusumkar@gmail.com>
2025-04-05 14:55:19 +0530

    new 1

--- abilitylink-frontend/src/utils/api.jsx
+++ abilitylink-frontend/src/utils/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://abilitylinks-server.onrender.com/api",
   timeout: 10000, // 10 seconds timeout
 });
 
